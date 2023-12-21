ef4ebe724c8ca54a2483acc2a61da9060b2871d9
arjan <arjan.peeters@hva.nl>
2023-12-21 17:25:52 +0100

    Polished enemy death anim, took longer then wanted. Added hat drop on death

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
-
+using System.Collections;
 public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 50f;
+    public GameObject playerHatPrefab;
+    public GameObject enemyHatPrefab;
+    public float destroyEnemyObjDelay = 4f;
     [SerializeField] private float currentHealth;
-
+    private bool isHitOnCooldown = false; 
+    [SerializeField] private float hitCooldownDuration = 1f; 
     private EnemyAnimatorHandler animatorHandler;
+    private bool isDead = false;
 
     void Start()
     {
@@ -17,18 +22,48 @@ public class EnemyHealth : MonoBehaviour
     {
         currentHealth -= damage;
 
-        animatorHandler.SetHit();
-
-        if (currentHealth <= 0)
+        if (currentHealth <= 0 && !isDead)
         {
             Die();
         }
+
+        if (!isHitOnCooldown) 
+        {
+            StartCoroutine(HitCooldown()); 
+            animatorHandler.SetHit();
+            Debug.Log("Enemt has been hit!");
+           
+        }
+    }
+
+    private IEnumerator HitCooldown()
+    {
+        isHitOnCooldown = true; 
+        yield return new WaitForSeconds(hitCooldownDuration);
+
+        isHitOnCooldown = false; 
     }
 
     void Die()
     {
+        isDead = true;
+        Debug.Log("Enemy has died!");
+       
         animatorHandler.SetDead();
+        if (enemyHatPrefab != null)
+        {
+            StartCoroutine(HatDropPositioningDelay());
 
-        Destroy(gameObject, 4f);
+        }
+        Destroy(gameObject, destroyEnemyObjDelay);
+    }
+
+    private IEnumerator HatDropPositioningDelay()
+    {
+        yield return new WaitForSeconds(destroyEnemyObjDelay -1);
+        
+        GameObject hat = Instantiate(playerHatPrefab, enemyHatPrefab.transform.position, Quaternion.identity);
+        enemyHatPrefab.SetActive(false);
+        
     }
 }
