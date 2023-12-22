9fbe74b1b8ad8890509b42747cf76f79ff0c654d
arjan <arjan.peeters@hva.nl>
2023-12-22 12:56:07 +0100

    Toggling flash

--- Assets/Flashlight.cs
+++ Assets/Flashlight.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Flashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(ToggleLights);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ToggleLights(ActivateEventArgs args)
     {
-        
+       GameObject.GetChildren().ForEach(child => child.SetActive(!child.activeSelf));
     }
 }
