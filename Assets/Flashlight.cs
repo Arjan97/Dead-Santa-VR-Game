09a418543d804252aa07a4d16a54714d4f0593e4
arjan <arjan.peeters@hva.nl>
2023-12-22 13:18:30 +0100

    Fixed flashlight error

--- Assets/Flashlight.cs
+++ Assets/Flashlight.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-
 public class Flashlight : MonoBehaviour
 {
+    private bool lightOn = false;
     void Start()
     {
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(ToggleLights);
     }
-
     private void ToggleLights(ActivateEventArgs args)
     {
-       GameObject.GetChildren().ForEach(child => child.SetActive(!child.activeSelf));
+        lightOn = !lightOn;
+        GameObject.Find("Flashlight").enabled = lightOn;
     }
 }
